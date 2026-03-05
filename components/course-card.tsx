4d1e7c73abf69a40e236b2d66ddcc1a359869ec8
Ajibade Faruk <ajibadefarukyoungprof02@gmail.com>
2026-03-05 12:48:29 +0100

    fixed

--- components/course-card.tsx
+++ components/course-card.tsx
@@ -10,38 +10,42 @@ function CourseCard() {
   const router = useRouter();
 
   return (
-    <section className="bg-[#FFFFFF] min-w-[280px] sm:min-w-70 max-w-[320px] sm:max-w-117 shrink-0 rounded-bl-md rounded-br-md rounded-tl-md rounded-tr-md">
-      <div className="relative">
-        <div className="w-full relative h-56 sm:h-70 md:h-80 ">
-          <Image
-            src={CardImage}
-            alt="Course Card"
-            fill
-            className="object-cover rounded-t-md"
-          />
-        </div>
-        <div className="absolute top-3 left-3">
-          <p className="bg-[#14AE5C] text-white p-5 w-max py-1 text-xs font-bold rounded-md">
-            Beginner
-          </p>
-        </div>
+    <section className="bg-[#FFF9ED] min-w-[280px] sm:min-w-70 max-w-[320px] sm:max-w-117 shrink-0 rounded-bl-md rounded-br-md rounded-tl-md rounded-tr-md">
+      <div className="w-full relative h-56 sm:h-70 md:h-80 ">
+        <Image
+          src={CardImage}
+          alt="Course Card"
+          fill
+          className="object-cover rounded-t-md"
+        />
       </div>
 
-      <div className=" items-center justify-between px-4 sm:px-5">
-        <h2 className="font-semibold text-[18px] sm:text-[20px] md:text-[22px] text-black mt-3">
-          Total Electrical 20 New
-        </h2>
-        <p className="text-[#9A9A9A] font-normal text-xs">ELECTRICAL</p>
-      </div>
-      <div className=" items-center justify-between px-4 sm:px-5 mt-7">
-        <h2 className="font-semibold text-[18px] sm:text-[20px] md:text-[22px] text-[#14AE5C] ">
-          £1,430 <span className="font-normal"> (£1630 + VAT)</span>
-        </h2>
-        <p className="text-black font-semibold text-xs flex gap-1">
-          Course duration:{" "}
-          <span className="text-[#9A9A9A] font-normal">2 weeks</span>
+      <h2 className="font-semibold text-[18px] sm:text-[20px] md:text-[22px] text-black mt-3 px-4 sm:px-5">
+        Course Title
+      </h2>
+
+      <div className="flex items-center justify-between mt-4 sm:mt-5 mb-5 sm:mb-7 px-4 sm:px-5">
+        <p className="text-[#9A9A9A] font-semibold text-sm sm:text-base">
+          Course Description
         </p>
+        <Link
+          href="#"
+          className="bg-[#627080] border border-[#9A9A9A] text-white font-semibold text-[10px] sm:text-xs px-3 sm:px-4 py-1"
+        >
+          More Information
+        </Link>
       </div>
+
+      <hr className="py-3 text-[#D2C4B3]" />
+
+      <p className="px-4 sm:px-5 text-sm sm:text-base">
+        These comprehensive Total Electrical 20 training courses are aimed at
+        non-electrical and electrical persons wishing to up-skill or re-train.
+        The aim of the Total Electrical 20 training course is to provide a level
+        of competence, which will allow work to be safely and efficiently
+        carried out on Electrical Equipment and Systems all within a 4 week
+        period.
+      </p>
 
       <div className="mx-4 sm:mx-5 pb-4 sm:pb-5">
         <Button className="bg-[#14AE5C] w-full h-12 sm:h-14.25 uppercase mt-6 sm:mt-8 text-sm sm:text-base">
